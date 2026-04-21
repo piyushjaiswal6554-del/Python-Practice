9a1f42c7403153b7db2a14cb3f9b14d97756fc01
piyushjaiswal6554-del <piyushjaiswal6554@gmail.com>
2026-04-21 10:18:57 +0530

    Day 3 - Calculator Project

--- Day3_Calculator.py
+++ Day3_Calculator.py
@@ -1,6 +1,18 @@
 print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
 choice=int(input("Enter your choice"))
 a=int(input("Enter your first number:" ))
 b=int(input("Enter your second number:" ))
 if choice==1:
 	print(a+b)
+elif choice==2:
+    print(a-b)
+elif choice==3:
+    print(a*b)
+elif choice==4:
+	if b!=0:
+		print(a/b)
+	else :
+		print ("cannot divide by zero")
